74582bf4d262b9f7607588ada2c7cf9d1b3b516d
dwialfian211 <dwialfian211@gmail.com>
2025-11-07 13:08:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// This file is unused when running index.html because the script is inline there.
+// Provided for convenience if you want to split files later.
+console.log('Use index.html (script inline)');
